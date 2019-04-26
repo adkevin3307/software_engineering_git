598ade867f197d8b392fc2273d2aec5d4bbc6361
adkevin3307 <adkevin3307@gmail.com>
2019-04-26 13:59:04 +0800

    CI/CD file

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return "infinity"
---
+++ TestCalculator.py
@@ -0,0 +1,33 @@
+import unittest
+from Calculator import *
+
+class TestCalculator(unittest.TestCase):
+    def test_int_add_1(self):
+        self.assertEqual(add(9, 3), 12)
+    
+    def test_float_add(self):
+        self.assertEqual(add(4.2, 3.1), 7.3)
+
+    def test_int_minus(self):
+        self.assertEqual(minus(9, 3), 6)
+
+    def test_float_minus(self):
+        self.assertEqual(minus(4.2, 3.2), 1.0)
+    
+    def test_int_multiply(self):
+        self.assertEqual(multiply(3, 6), 18)
+
+    def test_float_multiply(self):
+        self.assertEqual(multiply(1.2, 3.2), 3.84)
+
+    def test_int_divide(self):
+        self.assertEqual(divide(9, 3), 3)
+
+    def test_int_divide_2(self):
+        self.assertEqual(divide(9, 0), "infinity")
+    
+    def test_float_divide(self):
+        self.assertEqual(divide(9.3, 1.5), 6.2)
+
+if __name__ == '__main__':
+    unittest.main()
