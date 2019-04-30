daf04e6c818c9f3f1805725872465f40736afd6c
adkevin3307 <adkevin3307@gmail.com>
2019-04-30 21:42:00 +0800

    test flask

--- app_run.py
+++ app_run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'index page for software engineering'
+    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(debug = True)
